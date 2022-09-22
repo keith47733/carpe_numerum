931c575e493d82ae1e9e4a9e6f0f2bff6c92c51c
keith47733 <keith47733@gmail.com>
2022-09-22 17:03:25 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/carpe_numerum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carpe_numerum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage();
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> _numbers = [
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX',
+  ];
+  String _chosenNumber = 'VIII';
+  bool _gameStage2 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'carpe numerum'.toUpperCase(),
+          style: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      //
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Text(
+                'Pick a number:',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              GridView.count(
+                scrollDirection: Axis.vertical,
+                crossAxisCount: 3,
+                padding: const EdgeInsets.fromLTRB(35, 20, 35, 20),
+                mainAxisSpacing: 40.0,
+                crossAxisSpacing: 40.0,
+                shrinkWrap: true,
+                children: List.generate(_numbers.length, (index) {
+                  return ElevatedButton(
+                    onPressed: _gameStage2 ? null : () => _chooseNumber(index),
+                    child: Text(
+                      _numbers[index],
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ],
+          ),
+          AnimatedOpacity(
+            opacity: _gameStage2 ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 500),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Your number is:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+                Text(
+                  _chosenNumber,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 88,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Would you like to play again?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: _playAgain,
+                      child: const Text(
+                        'Yes',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 35),
+                    ElevatedButton(
+                      onPressed: _playAgain,
+                      child: const Text(
+                        'No',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _chooseNumber(index) {
+    setState(() {
+      _chosenNumber = _numbers[index];
+      _gameStage2 = true;
+    });
+  }
+
+  void _playAgain() {
+    setState(() {
+      _gameStage2 = false;
+    });
+  }
+}
